a6ebae99b6eb6fe613eb6c46c55e1feaaecb69a9
Jerome Wu <jeromewus@gmail.com>
2018-02-05 15:11:46 +0800

    chore: use gh-pages to simplify process

---
+++ scripts/add-ssh-key.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $DIR/deploy.key.enc -out $DIR/deploy.key -d
+chmod 600 $DIR/deploy.key
+eval `ssh-agent -s`
+ssh-add $DIR/deploy.key
